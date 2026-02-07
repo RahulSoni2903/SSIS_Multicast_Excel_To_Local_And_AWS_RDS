b93bcf0dc584b84a61354da3ef034e85fc434874
RahulSoni2903 <tho3unknown7802@gmail.com>
2026-02-07 17:19:02 +0530

    Add files via upload

---
+++ 3 Excel With Transform/Database Task4.sql
@@ -0,0 +1,13 @@
+
+USE SSISdest
+
+CREATE DATABASE TASKS;
+-- 3Excel File With Transformation 
+
+SELECT @@SERVERNAME;
+
+
+SELECT * FROM Task4;   ---- in Which Data Store Locally 
+
+
+
---
+++ 3 Excel With Transform/RDS File.sql
@@ -0,0 +1,16 @@
+
+
+
+CREATE DATABASE TASK4;
+USE TASK4;
+----------- Sorting Based on First name ----------------------- And then Merge 
+CREATE TABLE Cloud (
+    [id] float,
+    [First_name ] nvarchar(255),
+    [Last_name] nvarchar(255),
+    [gender] nvarchar(255),
+    [Phon_Company] nvarchar(255),
+    [month] nvarchar(255)
+)
+
+SELECT * FROM Cloud;
